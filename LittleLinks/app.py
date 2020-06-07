23ad3bd84cdcb4bf731deb231c1b396e604245c5
MACmidiDev <macmidi.webdesigns@gmail.com>
2020-06-06 23:41:26 -0400

    DCM3 | App Build - LiveLearn - Jitsi_video_API

--- LittleLinks/app.py
+++ LittleLinks/app.py
@@ -3,10 +3,9 @@ from flask import Flask, render_template
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-from os import path
-if path.exists("env.py"):
-  import env
-
+if os.path.exists("env.py"):
+  import env as config
+  
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = os.environ.get('MONGO_DBNAME')
@@ -14,15 +13,13 @@ app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 mongo = PyMongo(app)
 
 @app.route('/')
-
+@app.route('/live-learn')
+def landing():
+    return render_template("liveLearn.html")                           
 @app.route('/landing')
 def landing():
-    return render_template("landing.html")
-
-@app.route('/get_linkLearning')
-def get_linkLearning():
-    return render_template("linkLearning.html", 
-                           linkLearning=mongo.db.learnLink.find())
+    return render_template("landing.html", 
+                           LearningLinks=mongo.db.learnLink.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
