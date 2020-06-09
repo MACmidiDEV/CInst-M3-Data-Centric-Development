0875280ca11ffc91d4f312fafcd4ec7446fa5cae
MACmidiDev <macmidi.webdesigns@gmail.com>
2020-06-09 06:43:21 -0400

    DCM3 | App Build - LiveLearn - Landing|heroku

--- LittleLinks/app.py
+++ LittleLinks/app.py
@@ -1,10 +1,10 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 if os.path.exists("env.py"):
-  import env as config
+    import env
   
 
 app = Flask(__name__)
@@ -13,14 +13,12 @@ app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 mongo = PyMongo(app)
 
 @app.route('/')
-@app.route('/live-learn')
-def landing():
+@app.route('/home')
+def home():
+    return render_template("home.html")
+@app.route('/liveLearn')
+def liveLearn():
     return render_template("liveLearn.html")                           
-@app.route('/landing')
-def landing():
-    return render_template("landing.html", 
-                           LearningLinks=mongo.db.learnLink.find())
-
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=(os.environ.get('PORT')),
